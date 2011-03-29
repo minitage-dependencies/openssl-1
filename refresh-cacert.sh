0dbe29f920f306c7e559f06e723f9961fff0d95c
kiorky <kiorky@cryptelium.net>
2011-03-29 15:49:30 +0200

    up

--- refresh-cacert.sh
+++ refresh-cacert.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # stolen from the gentoo ca-certificates ebuild : ca-certificates-20090709.ebuild
 cd $(dirname $0)
+if [[ ! -f sys/share/minitage/minitage.env ]];then
+    ../../bin/paster create -t minitage.instances.env --no-interactive openssl-1
+fi
+. sys/share/minitage/minitage.env
 TOP=$PWD
 DATA="$TOP/ca-certificates"
 P=$TOP/parts/part/ssl
@@ -8,27 +12,25 @@ FN="ca-certificates_20090814+nmu3_all.deb"
 SRC="http://ftp.de.debian.org/debian/pool/main/c/ca-certificates/$FN"
 uco="$TOP/tmp/usr/sbin/update-ca-certificates"
 uc="$TOP/update-ca-certificates"
-CERTSCONF='$(dirname $0)/ca-certificates.conf'
-CERTSDIR='$(dirname $0)$/ca-certificates'
-LOCALCERTSDIR='$(dirname $0)/ca-certificates'
-CERTBUNDLE='ca-certificates.crt'
-ETCCERTSDIR='$(dirname $0)/parts/part/ssl/certs'
+
 sed=$(which gsed)
 if [[ -z $sed ]];then sed=sed;fi
 mkdir tmp
 cd tmp
 curl -O $SRC
-tar xjf $FN
+dpkg-deb -x $FN .
 tar xzf data.tar.gz
 rm -f control.tar.gz data.tar.gz debian-binary
 pushd usr/share/ca-certificates/
-find . -name '*.crt' | sort | cut -b3- | $sed -re s:^:$DATA/:g  > $TOP/parts/part/ssl/ca-certificates.conf
+# | $sed -re s:^:$DATA/:g 
+find . -name '*.crt' | sort | cut -b3-  > $TOP/ca-certificates.conf
 cp -rf . $DATA
 cp -f $uco $uc
-$sed -re "s:^CERTSCONF=.*:CERTSCONF=$CERTSCONF:g" -i $uc
-$sed -re "s:^CERTSDIR=.*:CERTSDIR=$CERTSDIR:g" -i $uc
-$sed -re "s:^LOCALCERTSDIR=.*:LOCALCERTSDIR=$LOCALCERTSDIR:g" -i $uc
-$sed -re "s:^CERTBUNDLE=.*:CERTBUNDLE=$CERTBUNDLE:g" -i $uc
-$sed -re "s:^ETCCERTSDIR=.*:ETCCERTSDIR=$ETCCERTSDIR:g" -i $uc
+$sed -re 's:^CERTSCONF=.*:cd $(dirname $0);TOP="$PWD";CERTSCONF=$TOP/ca-certificates.conf:g' -i $uc
+$sed -re 's:^CERTSDIR=.*:CERTSDIR=$TOP/ca-certificates:g' -i $uc
+$sed -re 's:^LOCALCERTSDIR=.*:LOCALCERTSDIR=$TOP/ca-certificates:g' -i $uc
+$sed -re 's:^ETCCERTSDIR=.*:ETCCERTSDIR=$TOP/etc/certs:g' -i $uc
+$sed -re 's:^HOOKSDIR=.*:HOOKSDIR=$TOP/tmp/etc/ca-certificates/update.d:g' -i $uc
+#$sed -re 's:^CERTBUNDLE=.*:CERTBUNDLE=$:g' -i $uc
 popd
 $uc
